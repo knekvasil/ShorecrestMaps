3e3c0a1dd2ea08c332b57791702fde2c31be9e49
Kalju Nekvasil <kajnekvasil@gmail.com>
2017-09-18 23:15:29 -0400

    initial commit

--- ShorecrestMaps/ViewController.swift
+++ ShorecrestMaps/ViewController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+//    let zoomLocation = CLLocation(latitude: 27.819024, longitude: -82.633971)
+    let regionRadius: CLLocationDistance = 1000
+    
+    let zoomLocation = CLLocationCoordinate2D(latitude: 27.819831, longitude: -82.631781)
+    
+//    func centerMapOnLocation(location: CLLocation){
+//        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius*2, regionRadius*2)
+//        mapView.setRegion(coordinateRegion, animated: false)
+//        
+//    }
+    
+    func rotateCamera(location: CLLocationCoordinate2D){
+
+        let camera = MKMapCamera(lookingAtCenter: location, fromDistance: regionRadius, pitch: 0, heading: 90)
+        mapView.setCamera(camera, animated: false)
+        mapView.mapType = .hybrid
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //centerMapOnLocation(location: zoomLocation) //MKCamera better, no rotation function in MKCoorReg
+        rotateCamera(location: zoomLocation)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    
 
 }
 
